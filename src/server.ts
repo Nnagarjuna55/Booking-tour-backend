813db92b6144cf88de5d3d1f7b8cc5390d4fe450
Nnagarjuna55 <nagarjuna93900@gmail.com>
2025-09-28 12:00:54 +0530

    m

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import app from "./app";
-import { config } from "./config/env";
+// import app from "./app";
+// import { config } from "./config/env";
 
-const PORT = config.port || 5000;
+// const PORT = config.port || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
+import cors from "cors";
+
+app.use(cors({
+  origin: "https://booking-tour-chi.vercel.app"
+}));
